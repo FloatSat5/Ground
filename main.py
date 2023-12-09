431911ee50ecb8865e0fe5d1c9117da379514a04
intsven <svenkunze99@gmail.com>
2023-12-09 14:49:14 +0100

    Add tabs

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import qdarkstyle
 import os
 # 1. Import QApplication and all the required widgets
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QAction, QTabWidget,QVBoxLayout, QPushButton
 
 
 def main():
@@ -16,8 +16,38 @@ def main():
     app.setStyleSheet(qdarkstyle.load_stylesheet())
     window.setWindowTitle("PyQt App")
     window.setGeometry(100, 100, 280, 80)
-    helloMsg = QLabel("<h1>Hello, World!</h1>", parent=window)
-    helloMsg.move(60, 15)
+    #helloMsg = QLabel("<h1>Hello, World!</h1>", parent=window)
+    #helloMsg.move(60, 15)
+    
+    layout = QVBoxLayout()
+    window.setLayout(layout)
+        
+    # Initialize tab screen
+    tabs = QTabWidget()
+    tabOverview = QWidget()
+    tabMission = QWidget()
+    tabSensors = QWidget()
+    tabCustom = QWidget()
+    tabs.resize(800,450)
+    tabs.move(0,0)
+    # Set text size of tabs
+    tabs.setStyleSheet("QTabBar::tab { height: 23px; width: 150px; font-size: 12pt; }")
+    
+    # Add tabs
+    tabs.addTab(tabOverview, "Overview")
+    tabs.addTab(tabMission, "Mission modes")
+    tabs.addTab(tabSensors, "Sensors")
+    tabs.addTab(tabCustom, "Custom")
+    
+    # Create first tab
+    tabOverview.layout = QVBoxLayout()
+    pushButton1 = QPushButton("PyQt5 button")
+    tabOverview.layout.addWidget(pushButton1)
+    tabOverview.setLayout(tabOverview.layout)
+    
+    # Add tabs to widget
+    layout.setContentsMargins(0, 0, 0, 0)
+    layout.addWidget(tabs, 0)
 
     # 4. Show your application's GUI
     window.show()
